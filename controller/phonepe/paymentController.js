9fa727e17d2fb209f3c3b9bac896014d0510f1b1
Nagaraj <nagaraj022000@gmail.com>
2023-11-21 17:32:20 +0530

    first commit

---
+++ controller/phonepe/paymentController.js
@@ -0,0 +1,191 @@
+const crypto =  require('crypto');
+const axios = require('axios');
+const nodemailer = require("nodemailer");
+// const {salt_key, merchant_id} = require('./secret')
+// console.log(process.env.salt_key);
+
+const newPayment = async (req, res) => {
+
+    try {
+    // console.log("Collections",req.body);
+
+        const merchantTransactionId = req.body.transactionId;
+        const data = {
+            merchantId: process.env.merchant_id,
+            merchantTransactionId: merchantTransactionId,
+            merchantUserId: req.body.MUID,
+            name: req.body.name,
+            amount: req.body.amount * 100,
+            redirectUrl: `https://cocoapods.onrender.com/api/status/${merchantTransactionId}`,
+            redirectMode: 'POST',
+            mobileNumber: req.body.number,
+            paymentInstrument: {
+                type: 'PAY_PAGE'
+            }
+        };
+
+        const payload = JSON.stringify(data);
+        const payloadMain = Buffer.from(payload).toString('base64');
+        const keyIndex = 1;
+        const string = payloadMain + '/pg/v1/pay' + process.env.salt_key;
+
+        const sha256 = crypto.createHash('sha256').update(string).digest('hex');
+        const checksum = sha256 + '###' + keyIndex;
+
+        const test_URL = "https://api-preprod.phonepe.com/apis/pg-sandbox/pg/v1/pay"
+        const prod_URL = "https://api.phonepe.com/apis/hermes/pg/v1/pay"
+
+        const options = {
+            method: 'POST',
+            url: prod_URL,
+            headers: {
+                accept: 'application/json',
+                'Content-Type': 'application/json',
+                'X-VERIFY': checksum
+            },
+            data: {
+                request: payloadMain
+            }
+        };
+
+        // console.log("Datas" , options);
+
+
+        axios.request(options).then(function (response) {
+            console.log(response.data)
+            return res.redirect(response.data.data.instrumentResponse.redirectInfo.url)
+        })
+
+
+        .catch(function (error) {
+            console.error(error);
+        });
+
+
+
+    } catch (error) {
+        res.status(500).send({
+            message: error.message,
+            success: false
+        })
+    }
+}
+
+const checkStatus = async(req, res) => {
+    const merchantTransactionId = res.req.body.transactionId
+    const merchantId = res.req.body.merchantId
+
+    const keyIndex = 1;
+    const string = `/pg/v1/status/${merchantId}/${merchantTransactionId}` + process.env.salt_key;
+    const sha256 = crypto.createHash('sha256').update(string).digest('hex');
+    const checksum = sha256 + "###" + keyIndex;
+
+    const options = {
+    method: 'GET',
+    url: `https://api.phonepe.com/apis/hermes/pg/v1/status/${merchantId}/${merchantTransactionId}`,
+    headers: {
+        accept: 'application/json',
+        'Content-Type': 'application/json',
+        'X-VERIFY': checksum,
+        'X-MERCHANT-ID': `${merchantId}`
+    }
+    };
+
+    axios.request(options).then(async(response) => {
+        // console.log(response.data)
+        if (response.data.success === true) {
+ 
+            const transporter = nodemailer.createTransport({
+                service: "gmail",
+                auth: {
+                    user: process.env.EMAIL,
+                    pass: process.env.PASSWORD
+                }
+            });
+            const mailOptions = {
+                from: process.env.EMAIL,
+                to: email ,  
+                subject: "Conformation",
+                html: `
+                <div style="display: flex; justify-content: center; width: 100%; height: 100vh;">
+                <div>
+                  <div style="display: flex; justify-content: center;">
+                      <img src="https://i.postimg.cc/50ZG7KZz/Moppp.png" style="width:100%; height: 50%;" />
+                  </div>
+          
+                  <div style="display: flex; justify-content:center;">
+                    <div style="width: 100%; background-color: black; color: white;">
+                    <div style="text-align: center;">
+                      <h2>Your order is on the way!</h2>
+                      <h4>Your order shipped</h4>
+                    </div>
+                      <div style="display: flex; justify-content: center; " >
+                      <div >
+                      <p><span style="color: #BA983C;">Name</span> : ${req.body.name}</p>
+          
+                      <p><span style="color: #BA983C;">Email</span> : ${req.body.email}</p>
+            
+                      <p><span style="color: #BA983C;">Number</span> : ${req.body.number}</p>
+          
+                      <p><span style="color: #BA983C;">Address</span> : ${req.body.address}</p>
+          
+                      <p><span style="color: #BA983C;">Gift</span> : ${req.body.final}</p>
+          
+             
+          
+                  </div>
+                      
+                  </div>
+                  </div>
+                  </div>
+                  </div>
+                </div>
+          </div>
+          
+            
+              `
+            };
+          
+            transporter.sendMail(mailOptions, (error, info) => {
+                if (error) {
+                    console.log("Error" + error)
+                } else {
+                    console.log("Email sent:" + info.response);
+                    res.status(201).json({status:201,info})
+                }
+          
+            })
+          
+
+
+            
+        }
+    })
+    .catch((error) => {
+        console.error(error);
+    });
+
+    // CHECK PAYMENT TATUS
+    axios.request(options).then(async(response) => {
+        console.log(response.data)
+        if (response.data.success === true) {
+            const url = `https://makeyourownchocolate.cocoa-pods.in/final`
+            return res.redirect(url)
+
+
+
+            
+        } else {
+            const url = `https://makeyourownchocolate.cocoa-pods.in/failiour`
+            return res.redirect(url)
+        }
+    })
+    .catch((error) => {
+        console.error(error);
+    });
+};
+
+module.exports = {
+    newPayment,
+    checkStatus
+}
---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const app = express();
+const port = 2000;
+const mongoose = require("mongoose")
+const cors = require("cors");
+require("dotenv").config();
+
+app.use(express.json());  
+app.use(cors({
+  origin : "https://makeyourownchocolate.cocoa-pods.in"
+}));
+const bodyParser = require('body-parser');
+
+mongoose.connect(process.env.DB)
+.then(()=>{
+    console.log("Database is connected");
+})
+.catch(()=>{
+    console.log("Database is Not connected");
+})
+
+const OrderSchemaRoom = mongoose.Schema({
+  titile:String,
+  titileTwo : String,
+  titileThree : String,
+  titileSel : String,
+  price : String,
+  image : String,
+  path : String,
+
+});
+const OrderConRoomList = mongoose.model('OrderRoom', OrderSchemaRoom);
+
+// Parse JSON and URL-encoded form data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })); 
+
+//Razorpay Route
+const phonepeRoute = require('./routes/phonepe/phonepeRoute')
+app.use("/api", phonepeRoute);
+
+
+app.get('/list-orders', async (req, res) => {
+  const orders = await OrderConRoomList.find();
+  res.send(orders);
+});
+
+// Starting Server
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
