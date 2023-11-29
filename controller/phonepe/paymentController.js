423c51fa2cfccdc04f9f20ad9919be01ad571f24
Nagaraj <124070128+Nagaraj02451@users.noreply.github.com>
2023-11-29 12:20:55 +0530

    Update paymentController.js

--- controller/phonepe/paymentController.js
+++ controller/phonepe/paymentController.js
@@ -5,9 +5,8 @@ const nodemailer = require("nodemailer");
 // console.log(process.env.salt_key);
 
 const newPayment = async (req, res) => {
-
+// console.log(req.body.GiftY);
     try {
-    // console.log("Collections",req.body);
 
         const merchantTransactionId = req.body.transactionId;
         const data = {
@@ -32,7 +31,7 @@ const newPayment = async (req, res) => {
         const sha256 = crypto.createHash('sha256').update(string).digest('hex');
         const checksum = sha256 + '###' + keyIndex;
 
-        const test_URL = "https://api-preprod.phonepe.com/apis/pg-sandbox/pg/v1/pay"
+        // const test_URL = "https://api-preprod.phonepe.com/apis/pg-sandbox/pg/v1/pay"
         const prod_URL = "https://api.phonepe.com/apis/hermes/pg/v1/pay"
 
         const options = {
@@ -52,7 +51,94 @@ const newPayment = async (req, res) => {
 
 
         axios.request(options).then(function (response) {
-            console.log(response.data)
+            // console.log(response.data.success)
+
+    
+                if(response.data.success === true){
+                    const transporter = nodemailer.createTransport({
+                        service: "gmail",
+                        auth: {
+                            user: process.env.EMAIL,
+                            pass: process.env.PASSWORD
+                        }
+                    });
+                    const mailOptions = {
+                        from: process.env.EMAIL,
+                        to: "nagaraj02022000@gmail.com",  
+                        subject: "Conformation",
+                        html: `
+                        <div style="display: flex; justify-content: center; width: 100%; height: 100vh;">
+                        <div>
+                          <div style="display: flex; justify-content: center;">
+                              <img src="https://i.postimg.cc/CKhDmJTC/about-us-1.png" style="width:100%; height: 45vh;" />
+                          </div>
+                  
+                          <div style="display: flex; justify-content:center;">
+                            <div style="width: 100%; background-color: black; color: white;">
+                            <div style="text-align: center;">
+                              <h2  style="color: #BA983C;">Your order is on the way!</h2>
+                              <h4  style="color: #BA983C;">Your order shipped</h4>
+                              <br />
+                            </div>
+                              <div style="width: 100%; height: 36vh; margin-left:10%;  " >
+                              <div >
+                              <p><span style="color: #BA983C;">Name</span>       :    ${req.body.CusName}</p>
+                  
+                              <p><span style="color: #BA983C;">Email</span>      :    ${req.body.Email}</p>
+                    
+                              <p><span style="color: #BA983C;">Number</span>     :    ${req.body.Number}</p>
+                              
+                              <p><span style="color: #BA983C;">Address</span>    :    ${req.body.Address}</p>
+
+                              <p><span style="color: #BA983C;">Gift</span>       :    ${req.body.GiftY}</p>
+                  
+                            
+                          </div>
+                              <div>
+                  
+                  
+                              <p><span style="color: #BA983C;">Tax</span>        :    ${req.body.tax}</p>
+
+                              <p><span style="color: #BA983C;">Rate</span>       :    ${req.body.Amount}</p>
+
+                              <p><span style="color: #BA983C;">Paid Amount</span>:    ${req.body.amount}</p>
+
+                              <p><span style="color: #BA983C;">Chocolate</span>  :    ${req.body.Final}</p>
+                
+                              <p><span style="color: #BA983C;">Fillings</span>   :    ${req.body.FinalTwo}</p>
+                              
+                              <p><span style="color: #BA983C;">Toppings</span>   :    ${req.body.FinalThree}</p>
+                  
+                         
+                  <br />
+                          </div>
+                          </div>
+                          </div>
+                          </div>
+                          </div>
+                        </div>
+                  </div>
+                  
+                    
+                      `
+                    };
+                  
+                    transporter.sendMail(mailOptions, (error, info) => {
+                        if (error) {
+                            
+                            console.log("Error" + error)
+                        } else {
+                            console.log("Email sent:" + info.response);
+                            res.status(201).json({status:201,info})
+                        }
+                  
+                    })
+                  
+                }
+               
+      
+           
+
             return res.send(response.data.data.instrumentResponse.redirectInfo.url)
         })
 
@@ -61,85 +147,7 @@ const newPayment = async (req, res) => {
             console.error(error);
         });
 
-        
-        axios.request(options).then(function (response) {
-            if (response.data.success === true) {
-                const transporter = nodemailer.createTransport({
-                    service: "gmail",
-                    auth: {
-                        user: process.env.EMAIL,
-                        pass: process.env.PASSWORD
-                    }
-                });
-                const mailOptions = {
-                    from: process.env.EMAIL,
-                    to: email ,  
-                    subject: "Conformation",
-                    html: `
-                    <div style="display: flex; justify-content: center; width: 100%; height: 100vh;">
-                    <div>
-                      <div style="display: flex; justify-content: center;">
-                          <img src="https://i.postimg.cc/CKhDmJTC/about-us-1.png" style="width:100%; height: 50%;" />
-                      </div>
-              
-                      <div style="display: flex; justify-content:center;">
-                        <div style="width: 100%; background-color: black; color: white;">
-                        <div style="text-align: center;">
-                          <h2>Your order is on the way!</h2>
-                          <h4>Your order shipped</h4>
-                        </div>
-                          <div style="display: flex; justify-content: center; " >
-                          <div >
-                          <p><span style="color: #BA983C;">Name</span> : ${req.body.CusName}</p>
-              
-                          <p><span style="color: #BA983C;">Email</span> : ${req.body.Email}</p>
-                
-                          <p><span style="color: #BA983C;">Number</span> : ${req.body.Address}</p>
-
-                          <p><span style="color: #BA983C;">Chocolate</span> : ${req.body.final}</p>
-              
-                        
-                      </div>
-                          <div>
-                          <p><span style="color: #BA983C;">Address</span> : ${req.body.address}</p>
-              
-                          <p><span style="color: #BA983C;">Gift</span> : ${req.body.yes}</p>
-              
-                          <p><span style="color: #BA983C;">Amount</span> : ${req.body.amount}</p>
-
-                          <p><span style="color: #BA983C;">Fillings</span> : ${req.body.finalTwo}</p>
-                          
-                          <p><span style="color: #BA983C;">Toppings</span> : ${req.body.FinalThree}</p>
-              
-                     
-              <br />
-                      </div>
-                      </div>
-                      </div>
-                      </div>
-                      </div>
-                    </div>
-              </div>
-              
-                
-                  `
-                };
-              
-                transporter.sendMail(mailOptions, (error, info) => {
-                    if (error) {
-                        console.log("Error" + error)
-                    } else {
-                        console.log("Email sent:" + info.response);
-                        res.status(201).json({status:201,info})
-                    }
-              
-                })
-              
-            }
-        })
-
-
-
+       
     } catch (error) {
         res.status(500).send({
             message: error.message,
@@ -173,10 +181,11 @@ const checkStatus = async(req, res) => {
     axios.request(options).then(async(response) => {
         console.log(response.data)
         if (response.data.success === true) {
-            const url = `https://makeyourownchocolate.cocoa-pods.in/final`
+            const url = `https://cocoapods.onrender.com/final`
             return res.redirect(url)
+            
         } else {
-            const url = `https://makeyourownchocolate.cocoa-pods.in/failed`
+            const url = `https://cocoapods.onrender.com/failed`
             return res.redirect(url)
         }
     })
